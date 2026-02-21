7f5a45d67c6c81502d68250c8b702da0e7084442
Rahul <rahulssolankii22@gmail.com>
2026-02-21 11:13:14 +0530

    updated main file

--- app/main.py
+++ app/main.py
@@ -12,3 +12,7 @@ app.include_router(bookings.router)
 @app.get("/")
 def root():
     return {"message": "Airbnb API running"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
